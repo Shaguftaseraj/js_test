6ba552f64f5786ec72e0820536cb8bf3bcf2179a
Shaguftaseraj <ahmadshagufta229@gmail.com>
2024-06-02 18:46:26 +0600

    js_TEST.js

--- js_TEST.js
+++ js_TEST.js
@@ -9,39 +9,45 @@ Assessment Requirements
 */
 
  //create a variable to hold your NFT's
-const NFTsarray = [];
-// this function will take in some values as parameters, create an
-// NFT object using the parameters passed to it for its metadata, 
-// and store it in the variable above.
-function mintNFT(_name, _eyecolor, _shirtType, _bling) {
-  const NFTobject = {
-    name: _name,
-    eyecolor: _eyecolor,
-    shirtType: _shirtType,
-    bling: _bling,
-  };
-  NFTsarray.push(NFTobject);
-  console.log("Person's name: " + _name);
-}
-// create a "loop" that will go through an "array" of NFT's
-// and print their metadata with console.log()
-function listNFTs() {
-  for (let i = 0; i < NFTsarray.length; i++) {
-    console.log("\nNo. " + (i + 1));
-    console.log("Name: " + NFTsarray[i].name);
-    console.log("Eye color: " + NFTsarray[i].eyecolor);
-    console.log("Shirt Type: " + NFTsarray[i].shirtType);
-    console.log("Bling: " + NFTsarray[i].bling);
-  }
-}
-// print the total number of NFTs we have minted to the console
-function getTotalSupply() {
-  console.log("\nTotal Supply =" + NFTsarray.length);
-}
-// call your functions below this line
-mintNFT("Shagufta", "Black", "Bodycon", "Diamond Ring");
-mintNFT("Ehtesham", "Blue", "Hoodie", "Hoops");
-mintNFT("Shahnawaz", "Brown", "Cargo", "Silver Chain");
-listNFTs();
-getTotalSupply();
-
+ const NFTsarray = [];
+ // this function will take in some values as parameters, create an
+ // NFT object using the parameters passed to it for its metadata, 
+ // and store it in the variable above.
+ function mintNFT(name,age,address,eyecolor,shirtType) {
+   const NFTobject = {
+     name: name,
+     age:age,
+     address: address,
+     eyecolor: eyecolor,
+     shirtType: shirtType,
+   };
+   NFTsarray.push(NFTobject);
+   console.log("Person's name: " + name);
+ }
+ // create a "loop" that will go through an "array" of NFT's
+ // and print their metadata with console.log()
+ function listNFTs() {
+   for (let i = 0; i < NFTsarray.length; i++) {
+     console.log("\n\n_____________________ PERSON DETAILS  _________________________\n\n");
+     console.log("\nNo. " + (i + 1));
+     console.log("Name: " + NFTsarray[i].name);
+     console.log("Age: " + NFTsarray[i].age);
+    console.log("Address: "+NFTsarray[i].address);
+     console.log("Eye color: " + NFTsarray[i].eyecolor);
+     console.log("Shirt Type: " + NFTsarray[i].shirtType);
+     console.log("\n\n_____________________* * * * *_________________________\n" );
+   }
+ }
+ // print the total number of NFTs we have minted to the console
+ function getTotalSupply() {
+   console.log("\nTotal Supply =" + NFTsarray.length);
+ }
+ // call your functions below this line
+ mintNFT("Shagufta", 19,"Delhi","Black", "Bodycon");
+ mintNFT("Ehtesham",22,"MP", "Blue", "Hoodie", "Hoops");
+ mintNFT("Shahnawaz",21,"UP", "Brown", "Cargo", "Silver Chain");
+ mintNFT("Bella",22,"Kolkata","Dark Blue","Polo");
+ mintNFT("Tanu",17,"Bangladesh","White","Full");
+ listNFTs();
+ getTotalSupply();
+ 
